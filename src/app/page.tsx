5e29ab805bdbe92701f740020e1b716ba2df27fb
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2023-12-20 14:42:46 -0300

    feat: add useQuery provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Content from "@/components/Content";
 
 export default function Home() {
   return (
-    <main className="h-screen bg-neutral-900">
+    <main className="min-h-screen bg-neutral-900">
       <div className="p-10">
         <div>
           <h1 className="text-5xl font-semibold text-white">Gitmate</h1>
-          <p className="text-xl text-neutral-300 mt-5">Type your Github username and spot those who are not following you</p>
+          <p className="text-xl text-neutral-300 mt-5">Type your Github username and spot those who are not following you back.</p>
         </div>
 
         <div className="mt-10">
