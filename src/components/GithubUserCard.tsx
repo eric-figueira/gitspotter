2b33a7682db85b996a40f4cc96cae8b430f3b46b
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2023-12-23 09:22:12 -0300

    refactor(userCard): fix design on mobile

--- src/components/GithubUserCard.tsx
+++ src/components/GithubUserCard.tsx
@@ -12,19 +12,19 @@ export default function GithubUserCard({ data }: IGithubUserCardProps) {
   return (
     <>
       <div className="flex flex-col md:flex-row gap-5 p-5 border-neutral-600 border-[2px] rounded-lg w-full">
-        <div className="w-fit">
-          <Avatar className="w-16 h-16">
+        <div className="flex justify-center items-center">
+          <Avatar className="w-20 h-20 border-2 border-emerald-600">
             <AvatarImage src={data.avatar_url} alt={data.name} />
             <AvatarFallback>
-              <Skeleton className="h-16 w-16 rounded-full" />
+              <Skeleton className="h-20 w-20 rounded-full" />
             </AvatarFallback>
           </Avatar>
         </div>
         <div>
-          <p className='text-xl text-neutral-300 w-fit mb-5'>{data.login}</p>
+          <p className='text-xl break-words text-neutral-300 text-center md:w-fit mb-5'>{data.login}</p>
           <a href={data.html_url} className="no-underline text-white" target="_blank">
             <Button
-              className="flex gap-2 max-sm:w-full lg:w-fit bg-emerald-600  hover:bg-emerald-600/70"
+              className="flex gap-2 max-sm:w-full w-fit bg-emerald-600  hover:bg-emerald-600/70"
               size='sm'
             >
               <span>Visit Profile</span>
